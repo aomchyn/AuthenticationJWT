d54d606ca565f044f332ebb32379822f68b3a479
aomchyn <orm.mee19@gmail.com>
2026-04-01 11:38:54 +0700

    Insert RestController,GetMapping for test api

--- src/main/java/com/authentication/user/UserApplication.java
+++ src/main/java/com/authentication/user/UserApplication.java
@@ -2,12 +2,20 @@ package com.authentication.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class UserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
+	}
+
+	@GetMapping("/test")
+	public String Test(){
+		return "Welcome to Spring boot for learn Authentication";	
 	}
 
 }
